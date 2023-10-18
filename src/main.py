3b916d659c9275dd2e809052dd60d2166b9b5d3c
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2023-10-17 23:39:47 -0300

    feat: rotas implementadas em aula

--- src/main.py
+++ src/main.py
@@ -1,17 +1,40 @@
-from fastapi import Body, FastAPI, Request, Response
+import json
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
-    return {"message": "Olá pessoal!"}
+def hello_world():
+    return {"message": "Hello World"}
 
 
-@app.post("/")
-async def criar_registro(request: Request, response: Response):
-    body = await request.body()
-    print(body)
-    response.body = body
-    response.status_code = 200
+@app.get("/{nome}")
+async def responda_nome(request: Request):
+    # Recupero os parâmetros enviados pelo path (barra de endereços)
+    params = request.path_params
+    # Recupero apenas o parâmetro nome
+    nome = params.get("nome")
+    # Crio o conteúdo de resposta (dict) e converto para string
+    response_content = json.dumps({"message": f"Olá {nome}"})
+    # Crio uma instancia da classe response, passando como parâmetros o conteúdo
+    # e o tipo de conteúdo (json)
+    response = Response(content=response_content,
+                        media_type="application/json",
+                        status_code=200)
+    # Devolvo a resposta
     return response
+
+
+@app.post("/outro")
+async def responda_outro(request: Request):
+    # Recupero os dados do body
+    body = await request.body()
+    # Converto o o body de bytes para dict {chave: valor}
+    body = json.loads(body)
+    # Recupero a propriedade nome
+    nome = body.get("nome")
+    # Recupero a propriedade valor
+    valor = body.get("valor")
+    # Retorno diretamente um dict, que é entendido como um retorno JSON
+    return {"message": f"Nome: {nome} - Valor: {valor}"}
